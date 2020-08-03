8ee21044bd281855d0cddcaf9a2c00b8d650e260
Yannis <yannis.bouacida@appliedtechnology.se>
2020-08-03 20:24:40 +0200

    price oracle for usd/eth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Web3 from "web3";
+import BigNumber from "bignumber.js";
+
+import "./App.css";
+
+const CHAINLINK_ORACLE_ABI = [
+  {
+    constant: true,
+    inputs: [],
+    name: "latestAnswer",
+    outputs: [
+      {
+        name: "",
+        type: "int256",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "latestTimestamp",
+    outputs: [
+      {
+        name: "",
+        type: "uint256",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+];
 
 function App() {
+  const [price, setPrice] = useState(null);
+  const [timeStamp, setTimeStamp] = useState(null);
+  const ORACLE_ADDRESS = "0xF79D6aFBb6dA890132F9D7c355e3015f15F3406F";
+
+  useEffect(() => {
+    const w3 = new Web3("https://cloudflare-eth.com");
+    let oracle = new w3.eth.Contract(CHAINLINK_ORACLE_ABI, ORACLE_ADDRESS);
+    oracle.methods.latestAnswer().call({}, function (error, res) {
+      if (error != null) {
+        console.log(error);
+        return;
+      }
+      setPrice(res);
+    });
+
+    oracle.methods.latestTimestamp().call({}, function (error, res) {
+      if (error != null) {
+        console.log(error);
+        return;
+      }
+      setTimeStamp(res);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Latest price from the oracle is: {price}</p>
+      <p>Latest timestamp was: {timeStamp}</p>
     </div>
   );
 }
